e70a21cb9846370ae94c4300545647c102272375
hack3rvaillant <hello@hacker-vaillant.org>
2023-06-07 13:07:44 +0200

    Install devbise and configure rspec

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,7 +1,15 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
+
+  config.generators do |g|
+    g.test_framework  :rspec, :fixture_replacement => :factory_girl, :views => true, :helper => false
+    g.fixture_replacement :factory_bot, :dir => 'spec/factories'
+    g.stylesheets     false
+    g.javascripts     false
+    g.helper          false
+  end
+  #  Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
@@ -70,4 +78,6 @@ Rails.application.configure do
 
   # Use memory store for bot sessions.
   config.telegram_updates_controller.session_store = :memory_store
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -65,3 +65,10 @@ RSpec.configure do |config|
 
   config.after { Telegram.bot.reset }
 end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
