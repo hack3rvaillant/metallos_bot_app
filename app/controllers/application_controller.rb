a6a1dac05b106cb0ec91530d51d2b48c0bb9f206
hack3rvaillant <hello@hacker-vaillant.org>
2023-06-21 22:26:09 +0200

    After sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
   # before_action :authenticate_user!
+  def after_sign_in_path_for(resource)
+    Avo::Engine.routes.url_helpers.resources_artists_path
+  end
 end
