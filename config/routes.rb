20793ee585727bd12f717f6698c095042a548ee9
hack3rvaillant <hello@hacker-vaillant.org>
2023-06-21 22:26:56 +0200

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
 Rails.application.routes.draw do
   authenticate :user do
-    mount Avo::Engine, at: Avo.configuration.root_path
+    scope :admin do
+      mount Avo::Engine, at: Avo.configuration.root_path
+    end
   end
+
+  root to: "pages#index"
 
   devise_for :users
   devise_scope :user do
     get "connexion", to: "devise/sessions#new"
     get "creation-de-compte", to: "devise/sessions#create"
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 
   telegram_webhook TelegramWebhooksController, :default
 end
