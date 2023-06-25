912202c32104415de14f7082ce0e8ae20ca4b7e3
hack3rvaillant <hello@hacker-vaillant.org>
2023-06-25 21:54:33 +0200

    Enforce auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  # before_action :authenticate_user!
+  before_action :authenticate_user!
   def after_sign_in_path_for(resource)
     Avo::Engine.routes.url_helpers.resources_artists_path
   end
