8570b0b8a633aa83863d2f24c9ecec354860ac6a
hack3rvaillant <hello@hacker-vaillant.org>
2023-06-25 21:55:29 +0200

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
 Rails.application.routes.draw do
-  authenticate :user do
-    scope :admin do
-      mount Avo::Engine, at: Avo.configuration.root_path
-    end
-  end
-
-  root to: "pages#index"
-
   devise_for :users
   devise_scope :user do
     get "connexion", to: "devise/sessions#new"
     get "creation-de-compte", to: "devise/sessions#create"
+  end
+
+  authenticate :user do
+    mount Avo::Engine, at: Avo.configuration.root_path
+  end
+
+  namespace :api do
+    resources :protocols, only: [:show, :index]
+    resources :bot_broadcasts, only: [:show, :index]
+    resources :events, only: [:show, :index]
   end
 
   telegram_webhook TelegramWebhooksController, :default
